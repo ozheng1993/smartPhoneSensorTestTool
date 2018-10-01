5bdc5c29d8e05c23d1cfd8f59a911bd45a1c7890
ou zheng <ouzheng@ous-macbook-pro.local>
2018-10-01 14:23:06 -0400

    Initial Commit

---
+++ sensorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sensorTest
+//
+//  Created by ou zheng on 10/1/18.
+//  Copyright © 2018 ou zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
